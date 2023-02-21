0caf8dd14edfad16a7af29cfc3c29abdba6ba40b
Roger <rogeryen@damaiapp.com.tw>
2023-02-21 17:07:00 +0800

    fix: remove code

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,8 +5,6 @@ const MONGODB_URL = process.env.MONGODB_URL;
 mongoose.set("strictQuery", false);
 mongoose.connect(MONGODB_URL)
 
-process.env.PORT
-
 db.on("error", () => console.log("mongo DB is error!"));
 db.once("open", () => console.log("mongo DB is connected!"));
 
